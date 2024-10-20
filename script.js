27ea10a28868a66482a88d620be8d76bd0333044
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-10-20 01:51:43 -0700

    script.js: •Made the information that shows up in the console also appear on the HTML page.
    main.css:
    •Cleaned up all unused CSS from the HTML page.

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ console.log("false == 0 =", false == 0);
 console.log("5 > 2 && 6 > 3 =", 5 > 2 && 6 > 3); 
 console.log('false == false ? "equal" : "not equal" =', false == false ? "equal" : "not equal"); 
 console.log("true || false =", true || false);
+
+function appendResult(text) {
+    const resultsDiv = document.getElementById('results');
+    const h2 = document.createElement('h2');
+    h2.textContent = text;
+    resultsDiv.appendChild(h2);
+}
+
+appendResult(`8 * null = ${8 * null}`); 
+appendResult(`"5" - 1 = ${"5" - 1}`); 
+appendResult(`"5" + 1 = ${"5" + 1}`); 
+appendResult(`"five" * 2 = ${"five" * 2}`); 
+appendResult(`false == 0 = ${false == 0}`); 
+appendResult(`5 > 2 && 6 > 3 = ${5 > 2 && 6 > 3}`); 
+appendResult(`false == false ? "equal" : "not equal" = ${false == false ? "equal" : "not equal"}`); 
+appendResult(`true || false = ${true || false}`);
